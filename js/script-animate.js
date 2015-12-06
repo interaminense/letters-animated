902bce0b74f4d2566f950041f5abbc8fac98e636
Adriano Interaminense <adriano.interaminense@gmail.com>
2015-12-06 13:28:13 -0200

    show code generate

--- js/script-animate.js
+++ js/script-animate.js
@@ -1,10 +1,14 @@
-var app = angular.module('app',[]);
+var app = angular.module('app',['ngSanitize']);
 
 app.controller('myController', ['$scope', function($scope){
 
   $scope.animationStyle = false;
+  $scope.showGenerate   = false;
 
   $scope.animate = function(textToAnimate){
+
+    $scope.showGenerate = true;
+    $scope.showCode     = false;
 
     var containerExibitionText = document.getElementsByClassName('container-exibition-text');
     $classOfInput = document.getElementsByClassName('class-of-input');
@@ -18,16 +22,22 @@ app.controller('myController', ['$scope', function($scope){
     $scope.textToAnimate = undefined;
     angular.element($classOfInput)[0].classList.remove('is-dirty');
 
+    $scope.print = "";
+
     for (var i = 0; i < textToAnimate.length; i++) {
       containerExibitionText[0].children[i].style.animationDelay = "0." + i + "s";
+      $scope.print += " .span:nth-child(" + ( i + 1 ) + ") { animation-delay: 0." + i + "s }";
       if(i > 9){
-        containerExibitionText[0].children[i].style.animationDelay = "1." + (i-10) + "s";
+        containerExibitionText[0].children[i].style.animationDelay = "1." + ( i - 10 ) + "s";
+        $scope.print += " .span:nth-child(" + ( i + 1 ) + ") { animation-delay: 0." + ( i - 10 ) + "s }";
       }
       if(i > 19){
-        containerExibitionText[0].children[i].style.animationDelay = "2." + (i-20) + "s";
+        containerExibitionText[0].children[i].style.animationDelay = "2." + ( i - 20 ) + "s";
+        $scope.print += " .span:nth-child(" + ( i + 1 ) + ") { animation-delay: 0." + ( i - 20 ) + "s }";
       }
       if(i > 29){
-        containerExibitionText[0].children[i].style.animationDelay = "3." + (i-30) + "s";
+        containerExibitionText[0].children[i].style.animationDelay = "3." + ( i - 30 ) + "s";
+        $scope.print += " .span:nth-child(" + ( i + 1 ) + ") { animation-delay: 0." + ( i - 30 ) + "s }";
       }
     }
 
