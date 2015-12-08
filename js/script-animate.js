911d719520a1c3104f2b43492086c919eaf4ccbf
Adriano Interaminense <adriano.interaminense@gmail.com>
2015-12-08 02:18:52 -0200

    improving usability and fixing bugs

--- js/script-animate.js
+++ js/script-animate.js
@@ -1,6 +1,6 @@
 var app = angular.module('app',['ngSanitize']);
 
-app.controller('myController', ['$scope', function($scope){
+app.controller('mainCtrl', ['$scope', function($scope){
 
   $scope.animationStyle = false;
   $scope.showGenerate   = false;
@@ -9,6 +9,8 @@ app.controller('myController', ['$scope', function($scope){
 
     $scope.showGenerate = true;
     $scope.showCode     = false;
+
+    $scope.stockText = textToAnimate;
 
     var containerExibitionText = document.getElementsByClassName('container-exibition-text');
     $classOfInput = document.getElementsByClassName('class-of-input');
@@ -22,23 +24,70 @@ app.controller('myController', ['$scope', function($scope){
     $scope.textToAnimate = undefined;
     angular.element($classOfInput)[0].classList.remove('is-dirty');
 
-    $scope.print = "";
-
     for (var i = 0; i < textToAnimate.length; i++) {
+      if(i >= 0 && i <= 9)
       containerExibitionText[0].children[i].style.animationDelay = "0." + i + "s";
-      $scope.print += " .span:nth-child(" + ( i + 1 ) + ") { animation-delay: 0." + i + "s }";
-      if(i > 9){
+      if(i >= 10 && i <= 19)
         containerExibitionText[0].children[i].style.animationDelay = "1." + ( i - 10 ) + "s";
-        $scope.print += " .span:nth-child(" + ( i + 1 ) + ") { animation-delay: 0." + ( i - 10 ) + "s }";
-      }
-      if(i > 19){
+      if(i >= 20 && i <= 29)
         containerExibitionText[0].children[i].style.animationDelay = "2." + ( i - 20 ) + "s";
-        $scope.print += " .span:nth-child(" + ( i + 1 ) + ") { animation-delay: 0." + ( i - 20 ) + "s }";
-      }
-      if(i > 29){
-        containerExibitionText[0].children[i].style.animationDelay = "3." + ( i - 30 ) + "s";
-        $scope.print += " .span:nth-child(" + ( i + 1 ) + ") { animation-delay: 0." + ( i - 30 ) + "s }";
-      }
+    }
+
+  };
+
+  $scope.generateCode = function(animationStyle){
+
+    console.log(animationStyle);
+
+    $scope.printHead = "";
+    $scope.printCss  = "";
+    $scope.printHtml = "";
+
+    $scope.showCode = true;
+
+    $scope.printHead += "&lt;head&gt;<br />  &lt;link rel='stylesheet' href='animations.css'&gt;<br />&lt;/head&gt;";
+
+    $scope.printHtml += "&lt;div class='is-animate " + animationStyle + "'&gt;<br />";
+
+    for (var i = 0; i < $scope.stockText.length; i++) {
+      $scope.printHtml += "  &lt;span&gt;" + $scope.stockText[i] + "&lt;/span&gt;<br />";
+    }
+
+    $scope.printHtml += "&lt;/div&gt;";
+
+    $scope.templates = {
+      "templateDefault" : ".is-animate span {<br />  animation-duration: 1s;<br />  animation-fill-mode: both;<br />  animation-iteration-count: infinite;<br />}<br /><br />",
+      "templateStyle1"  : ".is-animate.style1 span { animation-name: style1; }<br /><br />",
+      "templateStyle2"  : ".is-animate.style2 span { animation-name: style2; }<br /><br />",
+      "templateStyle3"  : ".is-animate.style3 span { animation-name: style3; }<br /><br />",
+      "templateStyle4"  : ".is-animate.style4 span { animation-name: style4; }<br /><br />",
+    };
+
+    switch (animationStyle) {
+      case "style1":
+      $scope.printCss += $scope.templates.templateStyle1;
+      break;
+      case "style2":
+      $scope.printCss += $scope.templates.templateStyle2;
+      break;
+      case "style3":
+      $scope.printCss += $scope.templates.templateStyle3;
+      break;
+      case "style4":
+      $scope.printCss += $scope.templates.templateStyle4;
+      break;
+    }
+
+    $scope.printCss += $scope.templates.templateDefault;
+
+    for (var i = 0; i < $scope.stockText.length; i++) {
+      console.log($scope.stockText.length);
+      if(i >= 0 && i <= 9)
+      $scope.printCss += ".is-animate span:nth-child(" + ( i + 1 ) + ") { animation-delay: 0." + i + "s }<br />";
+      if(i >= 10 && i <= 19)
+        $scope.printCss += ".is-animate span:nth-child(" + ( i + 1 ) + ") { animation-delay: 1." + ( i - 10 ) + "s }<br />";
+      if(i >= 20 && i <= 29)
+        $scope.printCss += ".is-animatespan:nth-child(" + ( i + 1 ) + ") { animation-delay: 2." + ( i - 20 ) + "s }<br />";
     }
 
   };
